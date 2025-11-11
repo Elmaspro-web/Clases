0af3aafeb5fd634c36e31d57bbb391e2e4c4e6f8
Elmaspro-web <ignaciolangarica68@gmail.com>
2025-11-11 17:01:47 +0100

    mejorado todo

--- Tema 4 JavaScript/task-manager-pro/js/patterns/TareaFacade.js
+++ Tema 4 JavaScript/task-manager-pro/js/patterns/TareaFacade.js
@@ -3,34 +3,88 @@ import {gestorTareas} from "./GestorTareas.js";
 class TareaFacade
 {
     constructor() {
-        const tareasGuardadas = localStorage.getItem("Tarea");
-        gestorTareas.tareas = tareasGuardadas ? JSON.parse(tareasGuardadas) : [];
+        this._loadLocalStorage();
     }
-    crearTarea(titulo, descripcion, prioridad) {
-        const tarea = gestorTareas.agregarTarea(titulo, descripcion, prioridad);
-        this._guardar();
-        return tarea;
+
+    _loadLocalStorage() {
+        gestorTareas.tareas = JSON.parse(localStorage.getItem("Tarea")) || [];
+    }
+
+    _addLocalStorage() {
+        localStorage.setItem("Tarea", JSON.stringify(gestorTareas.getTareas()));
+    }
+
+    crearTarea(titulo, descripcion, prioridad, estado = "No Completado") {
+        gestorTareas.agregarTarea(titulo, descripcion, prioridad, estado);
+        this._addLocalStorage();
     }
 
     completarTarea(indice) {
-        const tarea = gestorTareas.tareas[indice];
-        if (tarea) {
-            tarea.estado = tarea.estado === "Completada" ? "No completada" : "Completada";
-            this._guardar();
+        const tarea = gestorTareas.getTareas()[indice];
+        tarea.estado = tarea.estado === "Completada" ? "No completada" : "Completada";
+        this._addLocalStorage();
+
+    }
+
+    eliminarTarea(i) {
+        gestorTareas.eliminarTarea(i);
+        this._addLocalStorage();
+    }
+
+
+    mostrarTareas() {
+        const tarea = gestorTareas.getTareas()
+        let mTarea = this;
+        const seccionTareas = document.getElementById("idSectionTareas");
+        seccionTareas.innerHTML = "";
+        let contador = 0;
+        for (let i = 0; i < tarea.length; i++) {
+            const tareaCampo = tarea[i];
+            contador++;
+            const contenedor = document.createElement("div");
+            contenedor.classList.add("contenedorTarea");
+
+            if (tareaCampo.estado === "Completada") {
+                contenedor.style.backgroundColor = "green";
+                contenedor.style.color = "white";
+            } else if (tareaCampo.prioridad === "Low") {
+                contenedor.style.backgroundColor = "lightyellow";
+            } else if (tareaCampo.prioridad === "Normal") {
+                contenedor.style.backgroundColor = "lightgreen";
+            } else if (tareaCampo.prioridad === "High") {
+                contenedor.style.backgroundColor = "lightcoral";
+            }
+
+            contenedor.innerHTML = `
+            <p><strong>${contador}. Titulo: ${tareaCampo.titulo}</strong></p>
+            <p>Descripcion: ${tareaCampo.descripcion}</p>
+            <p>Prioridad: ${tareaCampo.prioridad}</p>
+            <p>Estado: ${tareaCampo.estado}</p>
+            <p>Fecha: ${tareaCampo.fechaCreacion}</p>
+        `;
+
+            const botonCompletar = document.createElement("button");
+            botonCompletar.textContent = "Completar";
+            botonCompletar.classList.add("botonCompletar");
+
+            botonCompletar.addEventListener("click", function () {
+                mTarea.completarTarea(i);
+                mTarea.mostrarTareas();
+            });
+
+            const botonEliminar = document.createElement("button");
+            botonEliminar.textContent = "Eliminar";
+            botonEliminar.classList.add("botonEliminar");
+
+            botonEliminar.addEventListener("click", function () {
+                mTarea.eliminarTarea(i);
+                mTarea.mostrarTareas();
+            });
+
+            contenedor.appendChild(botonCompletar);
+            contenedor.appendChild(botonEliminar);
+            seccionTareas.appendChild(contenedor);
         }
-    }
-
-    eliminarTarea(indice) {
-        gestorTareas.eliminarTarea(indice);
-        this._guardar();
-    }
-
-    getTareas() {
-        return gestorTareas.getTareas();
-    }
-    _guardar() {
-        const tarea = gestorTareas.getTareas() || [];
-        localStorage.setItem("Tarea", JSON.stringify(tarea));
     }
 }
 
