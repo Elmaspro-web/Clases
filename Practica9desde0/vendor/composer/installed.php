630315960070df3e015759ecf0ca1d2a3611cd38
Elmaspro-web <ignaciolangarica68@gmail.com>
2025-12-05 16:58:54 +0100

    1er Commit

---
+++ Practica9desde0/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nacho/practica9',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '777496c67df0d070f94132516ae2f1eb41e39fe7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nacho/practica9' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '777496c67df0d070f94132516ae2f1eb41e39fe7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
